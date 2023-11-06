35735479ff20d294cb29f1d42bb2c057e8b42601
paikpaik <woduf9311@gmail.com>
2023-11-06 19:10:20 +0900

    Add: SUM, MAX, MIN

---
+++ programmers/SUM, MAX, MIN/06. 중복 제거하기.sql
@@ -0,0 +1,17 @@
+-- Level 2
+-- 문제 설명
+/*
+ ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. ANIMAL_INS 테이블 구조는 
+다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 
+동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
+*/
+
+-- 문제
+/* 
+ 동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL 문을 작성해주세요. 이때 이름이 NULL인 
+경우는 집계하지 않으며 중복되는 이름은 하나로 칩니다.
+*/
+
+-- 정답 코드
+SELECT COUNT(DISTINCT NAME) 
+FROM ANIMAL_INS
